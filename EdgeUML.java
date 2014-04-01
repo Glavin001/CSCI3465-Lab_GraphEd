1ad7e2f87fd9162b69d95a25037e1eef3ae995ab
Dylan Young <dylanyoungmeijer@gmail.com>
2014-04-01 19:43:11 -0300

    initial commit

---
+++ EdgeUML.java
@@ -0,0 +1,21 @@
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+
+public class EdgeUML extends AbstractEdge {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void draw(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Point2D aPoint) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
